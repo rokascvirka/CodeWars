362dead3b296d725971213912205b33c8e3f1cb9
RokasCvirka <rokas42@gmail.com>
2024-02-02 11:35:36 +0200

    add kata

---
+++ Are the numbers in order/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Are_the_numbers_in_order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr1 = new int[] { 1, 2, 4, 7, 19 };
+            var arr2 = new int[] { 1, 6, 10, 18, 2, 4, 20 };
+
+            Console.WriteLine(IsAscOrder(arr1));
+            Console.WriteLine(IsAscOrder(arr2));
+
+
+
+        }
+        public static bool IsAscOrder(int[] arr)
+        {
+            return arr.SequenceEqual(arr.OrderBy(x => x));
+        }
+
+
+    }
+}
