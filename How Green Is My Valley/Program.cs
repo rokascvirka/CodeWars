546a5ff13a7ccacb37a809c35ed8ee00c3aac2d8
RokasCvirka <rokas42@gmail.com>
2024-02-02 09:06:09 +0200

    Add new projects

---
+++ How Green Is My Valley/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Cats_and_shelves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = 1;
+            var finish = 5;
+            var diff = finish - start;
+            var trippleJump = diff / 3;
+            var remainder = diff % 3;
+
+            var totalJumpsNeeded = trippleJump;
+
+            if(remainder == 1 || remainder == 2)
+            {
+                totalJumpsNeeded += remainder;
+            }
+
+            Console.WriteLine(totalJumpsNeeded); 
+
+        }
+    }
+}
