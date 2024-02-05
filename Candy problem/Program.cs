b4a4ae9a88f51ea5bc7e17d02807933ae3c53aa2
RokasCvirka <rokas42@gmail.com>
2024-02-05 12:07:06 +0200

    add kata

---
+++ Candy problem/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Candy_problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = GetMissingCandies(new int[] { 5, 8, 6, 4 });
+            Console.WriteLine(answer);
+        }
+
+        //smart solution
+        public static int GetMissingCandies_V2(int[] candies)
+        {
+            if (candies.Count() <= 1) return -1;
+            var max = candies.Max();
+            return candies.Select(x => max - x).Sum();
+        }
+
+        //my first solution
+        public static int GetMissingCandies(int[] candies) 
+        {
+            if(candies.Length <= 1)
+            {
+                return -1;
+            }
+            var mostCandies = candies.ToList().Max();
+            var candiensNeeded = 0;
+            foreach( var cand in candies)
+            {
+                var candiesToAdd = mostCandies - cand;
+                candiensNeeded += candiesToAdd;
+            }
+            return candiensNeeded;
+        }
+    }
+}
