3494c5fc1a92048ad04dd06fcf7532b69b260af0
RokasCvirka <rokas42@gmail.com>
2024-02-06 10:25:54 +0200

    add kata

---
+++ Suzuki needs help lining up his students/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Suzuki_needs_help_lining_up_his_students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = LineupStudents("Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi");
+            Console.WriteLine(answer);
+        }
+        public static String[] LineupStudents(String a)
+        {
+            var answer = a.Split(' ')
+                .OrderByDescending(x => x.Length)
+                .ThenByDescending(x => x)
+                .ToArray();
+            return answer;
+        }
+    }
+}
