9831be6e85100b2ae19b70d3b1b5ecb401e7662d
RokasCvirka <rokas42@gmail.com>
2024-02-06 10:18:05 +0200

    add kata

---
+++ Find the middle element/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Find_the_middle_element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = Gimme(new double[] { 5, 10, 14});
+            Console.WriteLine(answer);
+        }
+        public static int Gimme_V2(double[] inputArray)
+        {
+            return Array.IndexOf(inputArray, inputArray.OrderBy(x => x).ToArray()[1]);
+        }
+        public static int Gimme(double[] inputArray)
+        {
+            var cloned = inputArray.Clone() as double[];
+            Array.Sort(cloned);
+            var middleElement = cloned[1];
+
+            return Array.IndexOf(inputArray, middleElement);
+        }
+    }
+}
---
+++ Simple Fun #372 Lose Or Earn/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sum_of_array_singles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = Repeats(new List<int> { 9, 10, 19, 13, 19, 13 });
+            Console.WriteLine(answer);
+        }
+        public static int Repeats(List<int> source)
+        {
+            var answer = source.GroupBy(i => i)
+                .Where(x => x.Count() == 1)
+                .Select(x => x.Key)
+                .Sum();
+
+            return answer;
+        }
+    }
+}
